93eb6ad7ef9afb4a1cfc3cc906b25f320c6b8214
kai bergmann <kbde@riseup.net>
2024-01-19 18:05:31 +0100

    cahcing parks, updated login info shape

--- src/UI/Components/Card/Card.tsx
+++ src/UI/Components/Card/Card.tsx
@@ -2,10 +2,13 @@ import {ConcatClasses} from "@/Helpers/Formatting/ConcatClasses";
 import {ComponentPropsWithoutRef} from "react";
 import styles from './styles.module.css'
 
+export type CardBorder = "none" | "extraSmall" | "extraSmallTop" | "small" | "medium" |
+    "large" | "largeEnd" | "largeStart" | "largeTop" | "extraLarge" | "extraLargeTop" | "full";
 
-function Card({classes, children, ...props} : ComponentPropsWithoutRef<'div'> & {classes?: string | undefined}) {
+function Card({classes, children, BorderType = "small", ...props} : ComponentPropsWithoutRef<'div'> & {classes?: string | undefined, BorderType?: CardBorder}) {
+    const borderClass = styles[BorderType];
     return (
-        <div {...props} className={ConcatClasses(styles.border, classes)}>
+        <div {...props} className={ConcatClasses(styles.border, borderClass, classes)}>
             {children}
         </div>
     )
--- src/app/api/parks/route.ts
+++ src/app/api/parks/route.ts
@@ -1,5 +1,18 @@
 import {NextRequest, NextResponse} from "next/server";
+import {isTokenValid} from "@/app/api/helper";
+import {fakeDelayMS, parkConstants} from "@/app/api/parks/parkConstants";
+import {Delay} from "@/Helpers/Networking/Delay";
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json({});
+    const isAuthorized= await isTokenValid();
+    if (typeof isAuthorized !== "boolean")
+        return isAuthorized;
+
+    await Delay(fakeDelayMS);
+    const data = parkConstants;
+
+    return NextResponse.json({
+        ok: true,
+        parks: data
+    });
 }
